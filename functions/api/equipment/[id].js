01316d0a38ced8778e11c427e80012fb67e11663
baileywilson2305-code <bailey.wilson2305@gmail.com>
2026-08-31 12:02:41 +1000

    Add PUT (edit) support for equipment

--- functions/api/equipment/[id].js
+++ functions/api/equipment/[id].js
@@ -1,4 +1,27 @@
-import { json } from '../_utils.js';
+import { json, errorResponse, num, str, readJson } from '../_utils.js';
+
+export async function onRequestPut({ params, request, env }) {
+  const body = await readJson(request);
+  if (!body || !str(body.date) || !str(body.item)) {
+    return errorResponse('date and item are required');
+  }
+
+  await env.DB.prepare(
+    `UPDATE equipment SET date = ?, item = ?, cost = ?, notes = ?, wattage = ?, hours_per_day = ? WHERE id = ?`
+  )
+    .bind(str(body.date), str(body.item), num(body.cost), str(body.notes), num(body.wattage), num(body.hoursPerDay), params.id)
+    .run();
+
+  return json({
+    id: params.id,
+    date: str(body.date),
+    item: str(body.item),
+    cost: num(body.cost),
+    notes: str(body.notes),
+    wattage: num(body.wattage),
+    hoursPerDay: num(body.hoursPerDay),
+  });
+}
 
 export async function onRequestDelete({ params, env }) {
   await env.DB.prepare('DELETE FROM equipment WHERE id = ?').bind(params.id).run();
